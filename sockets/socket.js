ee3935ab23b9b640dc6dd3314b6295954ea6dd4c
Ariel Bernardo <arielbernardo@hotmail.com>
2021-09-14 10:14:21 -0300

    server recibiendo video

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,38 +1,26 @@
 const {io} = require('../index');
 
-// Sockets messages
+// Sockets messages 
 io.on('connection', (client) => {
     
+    //cuando un cliente se conecta
     console.log('Client logged in', client.id);
+    io.emit('sos:server',{id:client.id})
 
+    //cuando un cliente de desconecta
     client.on('disconnect', () => {
         console.log('Client logged out', client.id)
     });
-    
-    client.on('sos:message',(data)=>{
-        console.log(data);
-        client.emit('sos:server',data)
-    })
-
-    client.on('message', (payload) => { 
-        console.log('Payload: ', payload)
-        io.emit('message', {admin: 'New message!'})
-    });
-
-    client.on('new-message', (payload) => { 
-
-        io.emit('new-message', payload); // emits to every client
-        client.broadcast.emit('new-message', payload); // emits to every client except the one emiting
-    });
-
+  
+    //cuando el cliente envia un dato
     client.on('message-from-client', (payload) =>{
         
         console.log('New message:');
         console.log('From: ' + payload['name']);
         console.log('Message: ' + payload['message']);
         console.log('.===========================.');
-        io.emit('message-from-client', payload);
-       
     });
+    
+    
 
 });
