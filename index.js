824117677ad13b398d7911f1a75a2fc185717edc
Ariel Bernardo <arielbernardo@hotmail.com>
2021-09-15 17:00:22 -0300

    15-09-2021-final

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const express = require('express');
 const path = require('path');
+const app = express();
+const publicPath = path.resolve(__dirname, 'public');
 require('dotenv').config();
 
-// Express app
-const app = express();
-
-app.get('/server', (req, res) => {
-    res.sendFile(__dirname + '/server/index.html');
+// Midleware
+app.set('view engine', 'ejs')
+app.set('views',__dirname + '/views')
+// Public path
+app.use(express.static(publicPath));
+// console.log(publicPath);
+app.get('/:id',(req,res)=>{
+    valorId=req.params.id
+    res.render('index',{totem: valorId})
+})
+app.get('/:id/server', (req, res) => {
+    res.render('server',{comisaria: 4045})
 });
 
-app.get('/video', (req, res) => {
-    res.sendFile(__dirname + '/server/sos1.mp4');
-});
+// app.get('/:id/video', (req, res) => {
+//     res.sendFile(__dirname + '/sos1.mp4');
+// });
+
+
 // Node server
 const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
-
-
-
-// Public path
-const publicPath = path.resolve(__dirname, 'public');
-
-app.use(express.static(publicPath));
 
 
 server.listen(process.env.PORT, (err) => {
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,32 +1,41 @@
+const { Socket } = require('socket.io');
 const {io} = require('../index');
-
 // Sockets messages 
+
+var totems = {}
+
 io.on('connection', (client) => {
-    
+    // var reqUrl = document.getElementById('totem')
     //cuando un cliente se conecta
     console.log('Client logged in', client.id);
-    datos = {
-        id:client.id,
-        name:'Com.1',
-        message:'Se inicio apertura de camara.'
-    }
-    io.emit('sos:server',datos)
+    
 
     //cuando un cliente de desconecta
     client.on('disconnect', () => {
         console.log('Client logged out', client.id)
     });
   
-    //cuando el cliente envia un dato
+    //el servidor recibe el dato
     client.on('message-from-client', (payload) =>{
         
         console.log('New message:');
         console.log('From: ' + payload['name']);
-        console.log('Message: ' + payload['message']);
+        console.log('Message111: ' + payload['message']);
         console.log('.===========================.');
         io.emit('message-from-client',payload)
     });
+    client.on('sos:server', (data)=>{
+        io.emit('sos:server',data)
+    })
     
+    client.on('solved', (totem_name)=>{
+        io.to(totems[totem_name]).emit('solved')
+    })
+    client.on('totem_register', (totem_name)=>{
+        console.log("register");
+        totems[totem_name] = client.id;
+        client.emit('register_ok');
+    })
     
 
 });
