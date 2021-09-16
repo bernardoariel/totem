547ca8e44d7702d1438e0c1c85b933f290ee1a37
Ariel Bernardo <arielbernardo@hotmail.com>
2021-09-16 16:28:43 -0300

    Funcionando 16-9-2021

--- sockets/socket.js
+++ sockets/socket.js
@@ -2,40 +2,81 @@ const { Socket } = require('socket.io');
 const {io} = require('../index');
 // Sockets messages 
 
-var totems = {}
+var totems = [
+    {
+        "id":1,
+        "socket_id":'',
+        "name":'',
+        "state":false
+    },
+    {
+        "id":2,
+        "socket_id":'',
+        "name":'',
+        "state":false
+    }
+]
 
 io.on('connection', (client) => {
-    // var reqUrl = document.getElementById('totem')
+
     //cuando un cliente se conecta
     console.log('Client logged in', client.id);
     
-
     //cuando un cliente de desconecta
     client.on('disconnect', () => {
-        console.log('Client logged out', client.id)
+        console.log('Client logged out disconnect', client.id)
+        bsq_totem = totems.find( totem => totem.socket_id === client.id )
+        if(bsq_totem)   {
+            bsq_totem.socket_id='';
+            bsq_totem.state=false;
+            console.log('--------- desconectado -----',client.id)
+            console.log(totems)
+        }
+
+       
     });
-  
-    //el servidor recibe el dato
-    client.on('message-from-client', (payload) =>{
+    
+    client.on('totem_register', (totem_name)=>{
         
-        console.log('New message:');
-        console.log('From: ' + payload['name']);
-        console.log('Message111: ' + payload['message']);
-        console.log('.===========================.');
-        io.emit('message-from-client',payload)
-    });
+        totems[totem_name.id]['socket_id']=totem_name.socket_id;
+        totems[totem_name.id]['name']=totem_name.name;
+        totems[totem_name.id]['state']=true;
+        client.emit('register_ok');
+        console.log('LOG:SERVER:TOTEMS->',totems);
+    })
+
+
     client.on('sos:server', (data)=>{
         io.emit('sos:server',data)
     })
-    
+
     client.on('solved', (totem_name)=>{
-        io.to(totems[totem_name]).emit('solved')
-    })
-    client.on('totem_register', (totem_name)=>{
-        console.log("register");
-        totems[totem_name] = client.id;
-        client.emit('register_ok');
+        io.emit('solved2',totem_name)
+        
     })
     
+    client.on('totem:response', (data)=>{
+        console.log("activo",data);
+        // tomo el valor del totem name y le asigno el id de cliente
+        // totems[totem_name] = client.id;
+        //
+        //client.emit('register_ok');
+        // console.log(totems);
+    })
+
+    // setInterval(() => {
+      
+
+    //         for (const clave in totems) {
+    //             console.log('---------inicia----------------')
+                
+    //             console.log("clave->",totems[clave])
+    //             console.log("---------------------------------");
+    //             // console.log(io.to(totems[clave]).emit('connected'))
+    //             io.emit('response:activity',totems[clave]);
+    //         }
+
+      
+    // }, 10000);
 
 });
